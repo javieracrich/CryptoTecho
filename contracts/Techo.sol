4c794cec2098866d9f1b502991acd02c092fecc2
Javier <javier.1984@gmail.com>
2021-08-22 00:28:02 -0300

    remove library

--- contracts/Techo.sol
+++ contracts/Techo.sol
@@ -3,13 +3,11 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "./Enums.sol";
 
 contract Techo is Ownable, Pausable {
-    using Strings for uint256;
 
     IERC20 erc20;
     ContractStatus public contractStatus;
