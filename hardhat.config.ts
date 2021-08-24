a006dc6e8541447f74f7d234661c902dfab8b8a8
Javier <javier.1984@gmail.com>
2021-08-23 23:12:45 -0300

    deployment to ropsten network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,38 +1,34 @@
 import "@nomiclabs/hardhat-waffle";
-import { HardhatUserConfig, task } from "hardhat/config";
+import { task } from "hardhat/config";
 import "solidity-coverage";
 import "@nomiclabs/hardhat-ethers";
 require("@nomiclabs/hardhat-ganache");
+require("hardhat-deploy");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+
+const ALCHEMY_API_KEY = "...";
+const ROPSTEN_PRIVATE_KEY = "...";
+const ETHERSCAN_API_KEY = "...";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
-
   for (const account of accounts) {
     console.log(account.address);
   }
 });
 
-const config: HardhatUserConfig = {
-  // Your type-safe config goes here
-  // defaultNetwork: "ganache",
-  // networks: {
-  //     ganache: {
-  //         url: "http://172.17.144.1:7545",
-  //         // accounts: [privateKey1, privateKey2, ...]
-  //     }
-  // },
-};
-
-export default config;
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,23 @@
+const { Constants } = require("../test/Constants");
+const { utils } = require('ethers');
+const { ethers } = require("hardhat");
+
 async function main() {
+
+  const [deployer] = await ethers.getSigners();
+  console.log("deploying Techo with account: ", deployer.address);
+
+  console.log("Accout Balance: ", (await deployer.getBalance()).toString());
   // We get the contract to deploy
   const Techo = await ethers.getContractFactory("Techo");
 
-  const erc20Address = "";
-  const tenantAddress = "";
-  const landlordAddres = "";
-  const duration = ""
-  const amount = "";
-  const frequency = "";
-  const fee = "";
+  const erc20Address = "0xad6d458402f60fd3bd25163575031acdce07538d"; //ropsten dai
+  const tenantAddress = "0x...";
+  const landlordAddres = "0x...";
+  const duration = Constants.Year;
+  const amount = utils.parseEther("5000");
+  const frequency = Constants.Month;
+  const fee = "1";
 
   const techo = await Techo.deploy(erc20Address, tenantAddress, landlordAddres, duration, amount, frequency, fee);
 
